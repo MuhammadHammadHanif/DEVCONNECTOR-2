c0560a0fa1ec4191d1b39d0c4b8c19eb43b12644
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-15 07:07:22 +0500

    Create and apply action, reducer for alert

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -17,6 +18,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Register} />
